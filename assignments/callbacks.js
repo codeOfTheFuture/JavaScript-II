c6cf8b38c5fae3f7e48e6acd7a0c439a1ff8f595
Jeff Oliver <jeffoliver3384@gmail.com>
2019-05-07 15:44:30 -0400

    Higher order fuctions and callbacks task complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,27 +24,66 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+// Higher order function
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
+// Function invocation
+getLength(items, function(length) {
+  console.log(length);
+});
+
+// Higher order function
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
+// Function invocation
+last(items, function(lastItem) {
+  console.log(lastItem);
+});
+
+// Higher order function
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  const result = x + y;
+
+  return cb(result);
 }
 
+// Function invocation
+sumNums(5, 6, function(sum) {
+  console.log(sum);
+});
+
+// Higher order function
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  const result = x * y;
+
+  return cb(result);
 }
 
+// Function invocation
+multiplyNums(8, 7, function(product) {
+  console.log(product);
+});
+
+// Higher order function
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  let result;
+
+  if (list.includes(item)) {
+    result = true;
+  } else {
+    result = false;
+  }
+
+  cb(result);
 }
+
+contains('yo-yo', items, function(bool) {
+  console.log(bool);
+});
 
 /* STRETCH PROBLEM */
 
